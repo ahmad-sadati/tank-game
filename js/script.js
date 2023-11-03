56000d3acd52d697d2b6e0236bb5147cf8eefb17
Ahmad Sadati <a.sadati336@gmail.com>
2023-11-03 18:01:28 +0330

    initial shooting codes

--- js/script.js
+++ js/script.js
@@ -1,20 +1,61 @@
 const tank1 = document.querySelector(".tank1");
+const ball = document.querySelector(".ball");
 const tank2 = document.querySelector(".tank2");
+const divar1 = document.querySelector(".divar1");
 const playGround = document.querySelector(".bg");
-let turn;
+let turn, tank, divar, jahat, target;
+
 function gameInitialization() {
   tank1.style.left = "25px";
+  ball.style.left = "42px";
   tank1.style.top = "250px";
+  ball.style.top = "275px";
   tank2.style.right = "25px";
   tank2.style.top = "250px";
+  divar1.style.top = "100px";
+  divar1.style.left = "100px";
   turn = 1;
 }
-// const TP1 = tank1.style.top
-// const LP1 = tank1.style.left
-// console.log(TP1);
-// console.log(LP1);
 gameInitialization();
+divar = {
+  x: +divar1.style.left.replace(/px/g, ""),
+  y: +divar1.style.top.replace(/px/g, ""),
+  width: divar1.clientWidth,
+  height: divar1.clientHeight,
+};
+let timer = null;
+let aiPath = [
+  {
+    x: 12,
+    y: 45
+  },
+  {
+    x: 45,
+    y: 45
+  },
+  {
+    x: 90,
+    y: 45
+  },
+  {
+    x: 90,
+    y: 80
+  },
+]
 document.onkeydown = function (e) {
+  tank = {
+    x: +tank1.style.left.replace(/px/g, ""),
+    y: +tank1.style.top.replace(/px/g, ""),
+    width: tank1.clientWidth,
+    height: tank1.clientHeight,
+  };
+  // if (timer) {
+  //   clearTimeout(timer);
+  //   timer = null;
+  // }
+  // timer = setTimeout(() => {
+
+  // }, 200);
   switch (e.key) {
     case "ArrowUp":
       moveUp();
@@ -28,25 +69,32 @@ document.onkeydown = function (e) {
     case "ArrowRight":
       moveRight();
       break;
+    case " ":
+      shoot();
+      break;
   }
 };
 document.onkeyup = function (e) {
   tank1.classList.remove("shakeIt");
 };
 
-function moveUp() {
+function moveUp () {
+  jahat = 'up';
   const currentPosition = tank1.style.top.replace(/px/g, "");
   tank1.classList.remove("down");
   tank1.classList.remove("left");
   tank1.classList.remove("right");
   tank1.classList.add("up");
-  if (+currentPosition > 25) {
+  if (+currentPosition > 25 && !isCollide(tank,divar)) {
     tank1.style.top = +currentPosition - 20 + "px";
+    ball.style.top = +currentPosition - 20 + "px";
   } else {
     tank1.classList.add("shakeIt");
   }
 }
-function moveDown() {
+function moveDown () {
+  jahat = "down";
+  
   const currentPosition = tank1.style.top.replace(/px/g, "");
   tank1.classList.remove("up");
   tank1.classList.remove("left");
@@ -54,50 +102,68 @@ function moveDown() {
   tank1.classList.add("down");
   if (+currentPosition < playGround.clientHeight - 80) {
     tank1.style.top = +currentPosition + 20 + "px";
+    ball.style.top = +currentPosition + 20 + "px";
   } else {
     tank1.classList.add("shakeIt");
   }
 }
-function moveLeft() {
+function moveLeft () {
+  jahat = "left";
+  
   const currentPosition = tank1.style.left.replace(/px/g, "");
   tank1.classList.remove("up");
   tank1.classList.remove("down");
   tank1.classList.remove("right");
   tank1.classList.add("left");
-  if (+currentPosition > 25) {
+  if (+currentPosition > 25 && !isCollide(tank, divar)) {
     tank1.style.left = +currentPosition - 20 + "px";
+    ball.style.left = +currentPosition - 20 + "px";
   } else {
     tank1.classList.add("shakeIt");
   }
 }
-function moveRight() {
+function moveRight () {
+  jahat = "right";
+  
   const leftPosition = tank1.style.left.replace(/px/g, "");
-  // const topPostition = tank1.style.top.replace(/px/g, "");
+  const topPostition = tank1.style.top.replace(/px/g, "");
   tank1.classList.remove("up");
   tank1.classList.remove("down");
   tank1.classList.remove("left");
   tank1.classList.add("right");
-  // console.log(+topPostition);
-  console.log(tank1.style.left);
-  console.log(tank1.style.top);
-  if (
-    tank1.style.top < 340
-    // tank1.style.top > 40 &&
-    // tank1.style.left > 30 &&
-    // tank1.style.left < 50
-  ) {
-    tank1.classList.add("shakeIt");
-    console.log('first');
-  } else if (
-    (+leftPosition < playGround.clientWidth - 80
-       && +tank1.style.top > 340) ||
-    +tank1.style.top < 40 ||
-    +tank1.style.left < 30 ||
-    +tank1.style.left > 200
-  ) {
+  console.log(+topPostition);
+  if (+leftPosition < playGround.clientWidth - 80 && !isCollide(tank, divar)) {
     tank1.style.left = +leftPosition + 20 + "px";
+    ball.style.left = +leftPosition + 20 + "px";
   } else {
     tank1.classList.add("shakeIt");
-    console.log('second');
   }
+}
+function shoot () {
+  switch (jahat) {
+    case "up":
+      target = 0;
+      ball.style.top = 0 + "px";
+      break;
+    case "down":
+      target = playGround.clientHeight;
+      ball.style.top = target + "px";
+      break;
+    case "left":
+      target = 0;
+      ball.style.left = 0 + "px";
+      break;
+    case "right":
+      target = playGround.clientWidth;
+      ball.style.left = target + 'px'
+      break;
+  }
+}
+function isCollide(tank, divar) {
+  return !(
+    tank.y + tank.height < divar.y ||
+    tank.y > divar.y + divar.height ||
+    tank.x + tank.width < divar.x ||
+    tank.x > divar.x + divar.width
+  );
 }
